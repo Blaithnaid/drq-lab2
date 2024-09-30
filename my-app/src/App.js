956ecb807ca2e6cd0fcf1f7e19770a4afcbc28a2
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-09-30 14:19:58 +0100

    add content to header and footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <h1> Hello World! </h1>
-      <h2> It is currently {new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
