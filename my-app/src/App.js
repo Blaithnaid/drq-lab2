0e8225b0ef1f2534c669a04c0743d7265ba454d7
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-09-30 14:33:41 +0100

    setting up routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+//
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 // import the components for each section of the page
+import NavigationBar from "./components/NavigationBar.js";
 import Content from "./components/Content.js";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
