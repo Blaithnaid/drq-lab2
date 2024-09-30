617e871d8d33bd48e0d9883a6e2aaddcc317b190
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-09-30 14:21:49 +0100

    add components folder, move comps into it

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./components/Content.js";
+import "./components/Header.js";
+import "./components/Footer.js";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
