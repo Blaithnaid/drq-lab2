5a5ef7292746a723f26c2987abd35b3d133d176a
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-09-30 14:24:19 +0100

    fix imports

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import "./components/Content.js";
-import "./components/Header.js";
-import "./components/Footer.js";
+import Content from "./components/Content.js";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
