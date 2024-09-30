bbc743628ca69492e7443791385572bf8d55a706
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-09-30 14:27:53 +0100

    some comments, added bootstrap

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+
+// import the components for each section of the page
 import Content from "./components/Content.js";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 
 function App() {
   return (
+    // show each component on the page, within a div
     <div>
       <Header />
       <Content />
--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,6 +2,8 @@ import React from "react";
 
 const Content = () => {
   return (
+    // say hello world, then create a new date object and display the time by converting it to a string
+    // the toLocaleTimeString() method gets its locale from the JS runtime environment
     <div>
       <h1> Hello World! </h1>
       <h2> It is currently {new Date().toLocaleTimeString()}</h2>
